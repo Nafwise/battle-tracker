d539e1d839ccc4e8090567b57e6ab555b61b884c
Nafwise <n.d.cook91@gmail.com>
2025-12-06 03:53:12 +0000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const fireConfig = {
+  apiKey: "AIzaSyAdlWWNWbpizo_Dx2xJikTDKHdj4AJ0VSY",
+  authDomain: "battletracker-51502.firebaseapp.com",
+  projectId: "battletracker-51502",
+  storageBucket: "battletracker-51502.firebasestorage.app",
+  messagingSenderId: "717377728252",
+  appId: "1:717377728252:web:652210dc6904fef7ce7179"
+};
+
+window.__firebase_config = JSON.stringify(fireConfig);
+window.__app_id = "akashic-tome-suite";
